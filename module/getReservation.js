24410a1607336f1168f7c8e11a52349b8bbe8457
zoowb <zoowb0829@gmail.com>
2022-01-31 23:31:56 +0900

    :heavy_plus_sign: adding a dependency Co-authored-by: ssu00 <rachel4535@naver.com>

--- module/getReservation.js
+++ module/getReservation.js
@@ -1,34 +1,62 @@
 const Data = require("./reservation.json"); //예약 정보 json 파일
 const TwoWayData = require("./twoWay.json"); //왕복일 경우, 첫 번째 호출인지 두 번째 호출인지 구분하기 위함
 const TmapTimeMachine = require("./tmap.js"); //tmap 타임머신 경로 찾기
+const PreData = require("./preReservation.json"); //이전 예약된 정보 json 파일
+const CarData = require("./car.json"); //차량 정보 json 파일
 
 //서비스 id 이용해서 예약 정보 가져오기
 const GetReservation = async (id) => {
-  const a = Data.reservation[id]?.service == "basic" ? 10 : 20; //서비스 타입에 따라 정리 시간(a) 계산
+  const X = Data.reservation[id]?.service == "basic" ? 10 : 20; //서비스 타입에 따라 정리 시간(X) 계산
   const way = Data.reservation[id]?.way; //왕복인지, 편도인지, 편도라면 어떤 방향인지 나타내는 변수
   const idx = TwoWayData.twoWay.findIndex((i) => i.id == id);
   const idx2 = Data.reservation.findIndex((i) => i.id == id);
 
-  //편도(자택->병원)이거나 왕복 첫 번째 호출된 경우
-  if (
-    way == "to hospital" ||
-    (idx != -1 && TwoWayData.twoWay[idx]?.step == 0)
-  ) {
-    //예약 정보가 유효할 때 Tmap 타임머신 경로 찾기로 예상 소요시간 계산
+  //case1: 편도(자택->병원)
+  if(way=="to hospital"){
     if (idx2 != -1) {
-      let TmapTime = 0;
-      await TmapTimeMachine(Data.reservation[idx2]).then((tmapTime) => {
-        TmapTime = tmapTime + 20 + a; //소요시간 b 계산
+      let a1 = 0, b1 = 0, c1 = 0, d1 = 0;
+      const reservation=Data.reservation[idx2];
+      await TmapTimeMachine(reservation?.departureLon,reservation?.departureLat,reservation?.arrivalLon,reservation?.arrivalLat,"arrival",reservation?.hospitalArrival).then((tmapTime) => {
+        a1 = tmapTime + 20 + X; //a1
       });
-      return true;
+      b1 = reservation.hospitalArrival - a1;
+      
+      //--------여기서부터 시작!
+      // CarData.
+      // for(car){
+      //   for(res)
+      // }
+      // for()
+      // await TmapTimeMachine()
     }
   }
 
-  //편도(병원->자택)이거나 왕복 두 번째 호출된 경우
-  //아직 작업하기 전임
-  else {
-    return false;
-  }
+  // //편도(자택->병원)이거나 왕복 첫 번째 호출된 경우
+  // if (
+  //   way == "to hospital" ||
+  //   (idx != -1 && TwoWayData.twoWay[idx]?.step == 0)
+  // ) {
+  //   //예약 정보가 유효할 때 Tmap 타임머신 경로 찾기로 예상 소요시간 계산
+  //   if (idx2 != -1) {
+  //     let TmapTime = 0;
+  //     await TmapTimeMachine(Data.reservation[idx2]).then((tmapTime) => {
+  //       TmapTime = tmapTime + 20 + a; //소요시간 b 계산
+  //     });
+  //     return true;
+  //   }
+  // }
+
+  // //편도(병원->자택)이거나 왕복 두 번째 호출된 경우
+  // //아직 작업하기 전임
+  // else {
+  //   if (idx2 != -1) {
+  //     let TmapTime = 0;
+  //     await TmapTimeMachine(Data.reservation[idx2]).then((tmapTime) => {
+  //       TmapTime = tmapTime + 20 + a; //소요시간 b 계산
+  //     });
+  //     return false;
+  //   }
+  // }
 };
 
 module.exports = GetReservation;
--- module/tmapTimeMachine.js
+++ module/tmapTimeMachine.js
@@ -1,13 +1,24 @@
-// https://apis.openapi.sk.com/tmap/routes/prediction?version={1}&callback={callback} -> 타임머신 자동차 길 안내
-// version == API 버전 정보
-// resCoordType == 응답 좌표계 유형
-// reqCoordType == 요청 좌표계 유형
-// sort == 지리정보 개체 정렬순서
-// callback ==
-// totalValue == 응답 결과
+/*
+-------------------------------------
+----------TmapTimeMachine------------
+-------------------------------------
 
+departureLon : 출발지 경도
+
+departureLat : 출발지 위도
+
+arrivalLon : 도착지 경도
+
+arrivalLat : 도착지 위도
+
+predictionType : 출발 시간 구하기 or 도착 시간 구하기 타입 선택
+- "departure" : 출발시간 예측 길 안내.
+- "arrival" : 도착시간 예측 길 안내
+
+predictionTime : 희망 출발 시간 or 희망 도착 시간 입력
+*/
 const { default: axios } = require("axios");
-const TmapTimeMachine = async (addr) => {
+const TmapTimeMachine = async (departureLon,departureLat,arrivalLon,arrivalLat,predictionType,predictionTime) => {
   var headers = {};
   headers["appKey"] = "YOUR_APP_KEY"; //앱키 삽입해야 함
   headers["Content-Type"] = "application/json";
@@ -15,11 +26,6 @@ const TmapTimeMachine = async (addr) => {
   //value = "2" : 총 소요시간, 소요 거리만 확인하는 옵션
   var urlStr =
     "https://apis.openapi.sk.com/tmap/routes/prediction?version=1&reqCoordType=WGS84GEO&resCoordType=EPSG3857&format=json&totalValue=2";
-
-  const departureLon = addr?.departureLon; //출발지 위도
-  const departureLat = addr?.departureLat; //출발지 경도
-  const arrivalLat = addr?.arrivalLat; //도착지 위도
-  const arrivalLon = addr?.arrivalLon; //도착지 경도
 
   var data = JSON.stringify({
     routesInfo: {
@@ -35,8 +41,8 @@ const TmapTimeMachine = async (addr) => {
         lon: arrivalLon,
         lat: arrivalLat,
       },
-      predictionType: "departure", //출발지->도착지
-      predictionTime: "2022-01-30T12:28:22+0900", //예약 날짜, 시간
+      predictionType: predictionType, //출발지->도착지
+      predictionTime: predictionTime, //예약 날짜, 시간
       wayPoints: {
         wayPoint: [
           {
@@ -54,7 +60,7 @@ const TmapTimeMachine = async (addr) => {
   });
 
   //예상 소요시간 계산
-  let predictedTime = 0;
+  let estimatedTime = 0;
 
   //API에서 data받아오기
   try {
@@ -64,8 +70,8 @@ const TmapTimeMachine = async (addr) => {
       headers: headers,
       data: data,
     });
-    predictedTime = Math.round(res.data.features[0].properties.totalTime / 60); //tmap에서 계산한 시간에서 반올림(단위: 분)
-    return predictedTime;
+    estimatedTime = Math.round(res.data.features[0].properties.totalTime / 60); //tmap에서 계산한 시간에서 반올림(단위: 분)
+    return estimatedTime;
   } catch (err) {
     console.log(JSON.stringify(err));
   }
