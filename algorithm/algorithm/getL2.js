cc59911f6f3ee4a9c1404441e2d196ec2bc8302a
n3wzd <87504447+n3wzd@users.noreply.github.com>
2022-03-13 16:50:17 +0900

    fix 4

--- algorithm/algorithm/getL2.js
+++ algorithm/algorithm/getL2.js
@@ -5,9 +5,10 @@ const GetL2 = async (prevDepartureTimeArray, pickupTime) => {
   const L2 = [];
   for (let i = 0; i < prevDepartureTimeArray.length; i++) {
     const car_schedule = await getCarAdjRev(prevDepartureTimeArray[i].car_id, pickupTime);
+    const sch_prev_time = ToKoreanTime(new Date(car_schedule.prev.time));
     if (
       ToKoreanTime(prevDepartureTimeArray[i].prevDepartureTime) >
-      car_schedule.prev.time
+      sch_prev_time
     ) {
       L2.push(prevDepartureTimeArray[i]);
     }
